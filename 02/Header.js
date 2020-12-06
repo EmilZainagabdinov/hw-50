1ccc6df9ba192c5cd60b219a3823ba6a34561514
Emil Zainagabdinov <screw192@gmail.com>
2020-12-06 23:21:06 +0600

    Removed unnecessary attributes

--- 02/Header.js
+++ 02/Header.js
@@ -4,19 +4,19 @@ const Header = props => {
             <div className="container">
                 <ul className="nav justify-content-center">
                     <li className="nav-item">
-                        <a className="nav-link" href="#">{props.navlink1}</a>
+                        <a className="nav-link font-weight-bold" href="#">{props.navlink1}</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">{props.navlink2}</a>
+                        <a className="nav-link font-weight-bold" href="#">{props.navlink2}</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">{props.navlink3}</a>
+                        <a className="nav-link font-weight-bold" href="#">{props.navlink3}</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">{props.navlink4}</a>
+                        <a className="nav-link font-weight-bold" href="#">{props.navlink4}</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">{props.navlink5}</a>
+                        <a className="nav-link font-weight-bold" href="#">{props.navlink5}</a>
                     </li>
                 </ul>
             </div>
