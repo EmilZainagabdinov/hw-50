31b2d3a82b0109dfb1baa39231dce89d3a77f01b
Emil Zainagabdinov <screw192@gmail.com>
2020-12-06 23:25:23 +0600

    Created MainBlock component.

---
+++ 02/MainBlock.js
@@ -0,0 +1,11 @@
+const MainBlock = () => {
+    return (
+        <div className="col-10 row">
+            <ItemCard image="img/WORK_EMITZ.png" name="Work Gran Seeker CCX" sizes="19″ / 20″ / 21″" price="1359"/>
+            <ItemCard image="img/WORK_GRAN_SEEKER_CCX.jpg" name="Work Gran Seeker CCX" sizes="19″ / 20″" price="1289"/>
+            <ItemCard image="img/WORK_GNOSIS_CVX.jpg" name="Work Gran Seeker CCX" sizes="19″ / 20″ / 21″"
+                      price="1329"/>
+            <ItemCard image="img/WORK_EQUIP03.png" name="Work Gran Seeker CCX" sizes="14″ / 15″" price="899"/>
+        </div>
+    );
+};
