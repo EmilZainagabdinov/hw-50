3904610164442a2f4e2a44b784bda02fa7cb0e5d
Emil Zainagabdinov <screw192@gmail.com>
2020-12-06 17:45:06 +0600

    Edited block structure to use bootstrap styles

--- 01/moviecard.js
+++ 01/moviecard.js
@@ -1,9 +1,11 @@
 const Movie = props => {
     return (
-        <div className="movie">
-            <img src={props.poster} alt={props.name}/>
-            <h1>{props.name}</h1>
-            <p>Year: {props.year}</p>
+        <div className="movie card">
+            <img src={props.poster} className="card-img-top" alt={props.name}/>
+            <div className="card-body">
+                <h5 className="card-title">{props.name}</h5>
+                <p className="card-text">Год: {props.year}</p>
+            </div>
         </div>
     );
 };
