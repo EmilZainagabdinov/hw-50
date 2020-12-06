75e4eef710fad8432e0542485e76a086673feca3
Emil Zainagabdinov <screw192@gmail.com>
2020-12-06 17:29:23 +0600

    Moved IMG to the top of it's parent block.

--- 01/moviecard.js
+++ 01/moviecard.js
@@ -1,9 +1,9 @@
 const Movie = props => {
     return (
         <div className="movie">
+            <img src={props.poster} alt={props.name}/>
             <h1>{props.name}</h1>
             <p>Year: {props.year}</p>
-            <img src={props.poster} alt={props.name}/>
         </div>
     );
 };
