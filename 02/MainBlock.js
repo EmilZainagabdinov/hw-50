a4d27ae8b48853f074d5f67ad161db5aaafcbd0a
Emil Zainagabdinov <screw192@gmail.com>
2020-12-06 23:41:49 +0600

    Fixed wheel names

--- 02/MainBlock.js
+++ 02/MainBlock.js
@@ -1,11 +1,11 @@
 const MainBlock = () => {
     return (
         <div className="col-10 row">
-            <ItemCard image="img/WORK_EMITZ.png" name="Work Gran Seeker CCX" sizes="19″ / 20″ / 21″" price="1359"/>
+            <ItemCard image="img/WORK_EMITZ.png" name="Work Emitz" sizes="19″ / 20″ / 21″" price="1359"/>
             <ItemCard image="img/WORK_GRAN_SEEKER_CCX.jpg" name="Work Gran Seeker CCX" sizes="19″ / 20″" price="1289"/>
-            <ItemCard image="img/WORK_GNOSIS_CVX.jpg" name="Work Gran Seeker CCX" sizes="19″ / 20″ / 21″"
+            <ItemCard image="img/WORK_GNOSIS_CVX.jpg" name="Work Gnosis CVX" sizes="19″ / 20″ / 21″"
                       price="1329"/>
-            <ItemCard image="img/WORK_EQUIP03.png" name="Work Gran Seeker CCX" sizes="14″ / 15″" price="899"/>
+            <ItemCard image="img/WORK_EQUIP03.png" name="Work Equip03" sizes="14″ / 15″" price="899"/>
         </div>
     );
 };
