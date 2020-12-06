0e23b15515bf404802d2e8fbfc0248db486320e0
Emil Zainagabdinov <screw192@gmail.com>
2020-12-06 23:21:49 +0600

    Removed unnecessary attributes.

--- 02/Sidebar.js
+++ 02/Sidebar.js
@@ -1,16 +1,16 @@
 const Sidebar = props => {
     return (
-        <div className="col-3">
-            <div className="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-                <a className="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home"
+        <div className="col-2">
+            <div className="nav flex-column nav-pills">
+                <a className="nav-link active" href="#"
                    role="tab">{props.catLink1}</a>
-                <a className="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile"
+                <a className="nav-link" href="#"
                    role="tab">{props.catLink2}</a>
-                <a className="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages"
+                <a className="nav-link" href="#"
                    role="tab">{props.catLink3}</a>
-                <a className="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings"
+                <a className="nav-link" href="#"
                    role="tab">{props.catLink4}</a>
-                <a className="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings"
+                <a className="nav-link" href="#"
                    role="tab">{props.catLink5}</a>
             </div>
         </div>
